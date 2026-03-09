8e8383664f74a256d777f2f5ccbde3c2a4210759
RFJ26 <romeufgjanuario@gmail.com>
2026-03-09 10:01:21 +0000

    fix deploy

--- src/conexao.php
+++ src/conexao.php
@@ -6,25 +6,19 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 // Credenciais atualizadas com DB_PASS
-$host = '127.0.0.1'; 
-$user = 'root';
-$pass = ''; 
-$db   = 'fisio_estetica';
-
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS'); 
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 11494;
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try {
-    $conn = new mysqli($host, $user, $pass, $db );
+    $conn = new mysqli($host, $user, $pass, $db, $port );
     $conn->set_charset("utf8mb4");
 } catch (Exception $e) {
     error_log("Erro de Conexão: " . $e->getMessage());
     die("Erro técnico na base de dados. Verifique as variáveis de ambiente.");
 }
 
-/* // Credenciais atualizadas com DB_PASS
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS'); 
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT') ?: 11494; */
